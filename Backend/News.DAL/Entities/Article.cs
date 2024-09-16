3838539f2c1e1df3ee7cde2acc0ca92e8f060449
Savchuk Volodymyr <savchuk.v03@gmail.com>
2024-09-16 20:55:19 +0300

    Add db context, entities and initial migration

---
+++ Backend/News.DAL/Entities/Article.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace News.DAL.Entities;
+
+public class Article
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Content { get; set; } = string.Empty;
+
+    public string PictureUrl { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; }
+
+    [ForeignKey(nameof(Category))]
+    public Guid CategoryId { get; set; }
+
+    public Category Category { get; set; } = null!;
+
+    [ForeignKey(nameof(Author))]
+    public string AuthorId { get; set; } = string.Empty;
+
+    public IdentityUser Author { get; set; } = null!;
+}
---
+++ Backend/News.DAL/Storage/NewsContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using News.DAL.Entities;
+
+namespace News.DAL.Storage;
+
+public class NewsContext : IdentityDbContext<IdentityUser>
+{
+    public DbSet<Article> Articles { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    public NewsContext(DbContextOptions<NewsContext> options) : base(options)
+    { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        => base.OnConfiguring(builder);
+
+    protected override void OnModelCreating(ModelBuilder builder)
+        => base.OnModelCreating(builder);
+}
