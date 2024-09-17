2081690ed24c6348c0671b3d956f54c9c0580396
xach3110 <kazerog543@gmail.com>
2024-09-17 03:03:22 +0200

    Autorisation Window

--- Frontend/src/components/Topmenu/Topmenu.tsx
+++ Frontend/src/components/Topmenu/Topmenu.tsx
@@ -1,21 +1,56 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import logo from '../../image/logo_homepage.png';
 import globus from '../../image/Globus.png';
 import logining from '../../image/autor.png';
+import logo_autor from '../../image/autorisation_mage.png'
 import './Topmenu.css';
 
 interface TopmenuProps {}
 
-const Topmenu: FC<TopmenuProps> = () => (
-  <div className='centralB'>
-    <div className='imagesdiv'>
-      <img src={globus} className='item'></img>
-      <img src={logo} className='item'></img>
-      <img src={logining} className='item'></img>
-    </div>
+const Topmenu: FC<TopmenuProps> = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
 
-    <div className='stroke'></div>
-  </div>
-);
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <div className='centralB'>
+      <div className='imagesdiv'>
+        <img src={globus} className='item' alt="Globus" />
+        <img src={logo} className='item' alt="Logo" />
+        <img 
+          src={logining} 
+          className='item' 
+          alt="Login"
+          onClick={handleOpenModal} 
+        />
+      </div>
+
+      <div className='stroke'></div>
+
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close-button" onClick={handleCloseModal}>&times;</span>
+            
+            {/* Логотип */}
+            <img src={logo_autor} alt="Logo" className="modal-logo" />
+            
+            {/* Поле ввода */}
+            <input type="email" className="modal-input" placeholder="Введите Email"/>
+            
+            {/* Кнопка */}
+            <button className="modal-button">Next</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Topmenu;
