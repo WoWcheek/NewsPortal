473247108d030e3bede1956700845245681a4763
Savchuk Volodymyr <savchuk.v03@gmail.com>
2024-09-19 23:05:24 +0300

    Add auth requests

--- Frontend/src/components/Topmenu/Topmenu.tsx
+++ Frontend/src/components/Topmenu/Topmenu.tsx
@@ -1,55 +1,176 @@
-import React, { FC, useState } from 'react';
-import globus from '../../image/Globus.png';
-import logining from '../../image/autor.png';
-import logo_autor from '../../image/autorisation_mage.png'
-import './Topmenu.css';
+import React, { FC, useState } from "react";
+import globus from "../../image/Globus.png";
+import logining from "../../image/autor.png";
+import logo_autor from "../../image/autorisation_mage.png";
+import "./Topmenu.css";
 
 interface TopmenuProps {}
 
 const Topmenu: FC<TopmenuProps> = () => {
-  const [isModalOpen, setModalOpen] = useState(false);
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [isLogin, setIsLogin] = useState(true);
+    const [email, setEmail] = useState("");
+    const [nickname, setNickname] = useState("");
+    const [password, setPassword] = useState("");
 
-  const handleOpenModal = () => {
-    setModalOpen(true);
-  };
+    const handleOpenModal = () => {
+        setModalOpen(true);
+    };
 
-  const handleCloseModal = () => {
-    setModalOpen(false);
-  };
+    const handleCloseModal = () => {
+        setModalOpen(false);
+    };
 
-  return (
-    <div className='centralB'>
-      <div className='imagesdiv'>
-        <img src={globus} className='item' alt="Globus" />
-        <h3 className="footer-logo"><span className="highlight">News</span></h3>
-        <img 
-          src={logining} 
-          className='item' 
-          alt="Login"
-          onClick={handleOpenModal} 
-        />
-      </div>
+    const handleLoginSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
 
-      <div className='stroke'></div>
+        if (
+            nickname.length == 0 ||
+            password.length == 0 ||
+            (!isLogin && email.length == 0)
+        ) {
+            return;
+        }
 
-      {isModalOpen && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close-button" onClick={handleCloseModal}>&times;</span>
-            
-            {/* Логотип */}
-            <img src={logo_autor} alt="Logo" className="modal-logo" />
-            
-            {/* Поле ввода */}
-            <input type="email" className="modal-input" placeholder="Введите Email"/>
-            
-            {/* Кнопка */}
-            <button className="modal-button">Next</button>
-          </div>
+        if (isLogin) await sendLoginRequest(nickname, password);
+        else await sendRegisterRequest(email, nickname, password);
+    };
+
+    const sendLoginRequest = async (nickname: string, password: string) => {
+        const response = await fetch("https://localhost:7101/api/Auth/login", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ Username: nickname, Password: password })
+        });
+        const data = await response.json();
+
+        if (!response.ok) {
+            alert("Error occurred!");
+            return;
+        }
+
+        handleCloseModal();
+        localStorage.setItem("token", data.token);
+
+        alert("Logged in successfully!");
+
+        setEmail("");
+        setNickname("");
+        setPassword("");
+    };
+
+    const sendRegisterRequest = async (
+        email: string,
+        nickname: string,
+        password: string
+    ) => {
+        const response = await fetch(
+            "https://localhost:7101/api/Auth/register",
+            {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    Email: email,
+                    Username: nickname,
+                    Password: password
+                })
+            }
+        );
+
+        if (response.ok) {
+            alert("User was registered successfully!");
+            setIsLogin(true);
+            setEmail("");
+            setNickname("");
+            setPassword("");
+            return;
+        }
+
+        alert("Error occurred!");
+    };
+
+    return (
+        <div className="centralB">
+            <div className="imagesdiv">
+                <img src={globus} className="item" alt="Globus" />
+                <h3 className="footer-logo">
+                    <span className="highlight">News</span>
+                </h3>
+                <img
+                    src={logining}
+                    className="item"
+                    alt="Login"
+                    onClick={handleOpenModal}
+                />
+            </div>
+
+            <div className="stroke"></div>
+
+            {isModalOpen && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <span
+                            className="close-button"
+                            onClick={handleCloseModal}>
+                            &times;
+                        </span>
+
+                        <img
+                            src={logo_autor}
+                            alt="Logo"
+                            className="modal-logo"
+                        />
+
+                        <form onSubmit={handleLoginSubmit}>
+                            {!isLogin && (
+                                <input
+                                    type="email"
+                                    className="modal-input"
+                                    placeholder="Введите адрес електронной почты"
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                    required
+                                />
+                            )}
+                            <input
+                                type="text"
+                                className="modal-input"
+                                placeholder="Введите имя пользователя"
+                                value={nickname}
+                                onChange={e => setNickname(e.target.value)}
+                                required
+                            />
+                            <input
+                                type="password"
+                                className="modal-input"
+                                placeholder="Введите пароль"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                                required
+                            />
+
+                            <button type="submit" className="modal-button">
+                                {isLogin ? "Login" : "Register"}
+                            </button>
+                            <p
+                                className="toggle-status"
+                                onClick={() => setIsLogin(x => !x)}>
+                                {isLogin
+                                    ? "I don't have an account"
+                                    : "Back to login"}
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Topmenu;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import Topmenu from "./components/Topmenu/Topmenu.tsx";
-import Lastnews from "./components/Lastnews/Lastnews.tsx";
-import Down from "./components/Down/Down.tsx";
-import OneNewsAll from "./components/OneNewsAll/OneNewsAll.tsx"; // Импорт компонента для отображения одной новости
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Router>
-      <Topmenu />
-      <Routes>
-        <Route path="/" element={<Lastnews />} />
-        <Route path="/news/:id" element={<OneNewsAll />} />
-      </Routes>
-      <Down />
-    </Router>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
